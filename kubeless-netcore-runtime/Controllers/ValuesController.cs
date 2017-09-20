b8f81638707348aa46e17a3aaa665ece30bd2bfc
Allan Targino <altargin@microsoft.com>
2017-09-20 04:37:13 -0300

    adding netcore mock support

---
+++ kubeless-netcore-runtime/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace kubeless_netcore_runtime.Controllers
+{
+    [Route("/")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "Hey, CSE Rocks!";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+    }
+}
