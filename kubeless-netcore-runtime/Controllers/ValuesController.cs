243f7920bee51b816270beebb679df5a5cee9537
Allan Targino <altargin@microsoft.com>
2017-09-21 18:27:35 -0300

    getting function in runtime

--- kubeless-netcore-runtime/Controllers/ValuesController.cs
+++ kubeless-netcore-runtime/Controllers/ValuesController.cs
@@ -20,6 +20,31 @@ namespace kubeless_netcore_runtime.Controllers
             response.AppendLine($"MOD_NAME: {Environment.GetEnvironmentVariable("MOD_NAME")}");
             response.AppendLine($"FUNC_HANDLER: {Environment.GetEnvironmentVariable("FUNC_HANDLER")}");
 
+            //List files:
+            var directory = "/bin";
+            var files = System.IO.Directory.EnumerateFiles(directory);
+            response.AppendLine($"FILES IN {directory}: {files.Count()}");
+
+            try
+            {
+                //List files again:
+                directory = "/kubeless/";
+                files = System.IO.Directory.EnumerateFiles(directory);
+                response.AppendLine($"FILES IN {directory}: {files.Count()}");
+                foreach (var file in files)
+                {
+                    response.AppendLine($"FILE: {file}");
+                    response.AppendLine("CONTENT:");
+                    response.AppendLine(System.IO.File.ReadAllText(file));
+                    response.AppendLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                response.AppendLine($"ERROR: {ex.Message}");
+            }
+            
+            response.AppendLine("END");
             return response.ToString();
         }
 
