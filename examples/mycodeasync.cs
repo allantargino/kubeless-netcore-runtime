a68f4efd3961a4888aaa837aa13830a558751e93
Matthias Gernand <m.gernand@fluxera.com>
2018-01-21 09:46:30 +0100

    Extended the async example.

--- examples/mycodeasync.cs
+++ examples/mycodeasync.cs
@@ -1,11 +1,15 @@
 using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 
 public class mycodeasync
 {
+    private static readonly HttpClient client = new HttpClient();
+
     public async Task<string> execute(HttpRequest request)
     {
-        return await Task.FromResult("hello async world");
+        HttpResponseMessage result = await client.GetAsync("https://raw.githubusercontent.com/DXBrazil/kubeless-netcore-runtime/master/README.md");
+        return await result.Content.ReadAsStringAsync();
     }
 }
